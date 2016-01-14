85aff557940af677c4862e58fa2358acad668a21
Patrice Lebel <patrice.lebel@outlook.com>
2016-01-13 21:38:53 -0500

    topic touched (for search_cache refresh)

--- app/models/forem/post.rb
+++ app/models/forem/post.rb
@@ -7,7 +7,7 @@ module Forem
     # Used in the moderation tools partial
     attr_accessor :moderation_option
 
-    belongs_to :topic
+    belongs_to :topic, touch: true
     belongs_to :forem_user, :class_name => Forem.user_class.to_s, :foreign_key => :user_id
     belongs_to :reply_to, :class_name => "Post"
 
